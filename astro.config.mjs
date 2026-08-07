3c673bdd982dce5370e32eac943d04bfff9be4f3
Vlady <vladichoffx@gmail.com>
2026-08-07 12:11:05 -0500

    changed astro site url to correct domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // Replace this URL when you publish the portfolio.
-  site: 'https://your-domain.com',
+  site: 'https://ingvladimiro.com',
   output: 'static',
 });
